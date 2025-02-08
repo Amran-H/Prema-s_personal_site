bca7ffcc013b1fe3272519d5d61870c17fd1b95e
Amran Hossain <amran.h.akash@gmail.com>
2025-02-08 13:27:17 +0600

    services added

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                     <button>
                         <Image src={assets.moon_icon} alt='' className='w-6' />
                     </button>
-                    <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image alt='' src={assets.arrow_icon} className='w-3' /> </a>
+                    <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo font-semibold'>Contact <Image alt='' src={assets.arrow_icon} className='w-3' /> </a>
                     <button className='block md:hidden' onClick={openMenu}>
                         <Image src={assets.menu_black} alt='' className='w-6' />
                     </button>
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import About from "./Components/About";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
+import Services from "./Components/Services";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Header />
       <About />
+      <Services />
     </>
   );
 }
